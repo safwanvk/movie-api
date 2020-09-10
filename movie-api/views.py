796b8ea02af005fbd50b00d6eabac0454ff92758
safwanvk <safwanvalakundil@gmail.com>
2020-09-10 22:42:05 +0530

    :zap: Set get_movie

--- movie-api/views.py
+++ movie-api/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -24,8 +24,18 @@ def add_movie():
 
     db.session.add(new_movie)
     db.session.commit()
-    
+
     return 'Done', 201
+
+
+@app.route('/get_movie', methods=['GET'])
+def get_movie():
+    movie_list = Movie.query.all()
+    movies = []
+    for movie in movie_list:
+        movies.append({'title': movie.title, 'rating': movie.rating})
+
+    return jsonify({'moves': movies})
 
 
 if __name__ == '__main__':
